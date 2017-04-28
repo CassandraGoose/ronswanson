d41595b62730461ca884baae78d73d5cd1f3e7a3
Cassandra Torske <cassandra.torske@gmail.com>
2017-04-28 09:40:25 -0600

    stuff

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         $('#divChunk').empty()
         var theGif = results.data.image_url
         $('#divChunk').prepend(`<div class="row">
-              <div class="col s8 offset-s4 bottomsies">
+              <div class="col s10 offset-s2 bottomsies">
                 <div class="card cardcolor">
                   <div class="card-image">
                     <img class="max-img" src="${theGif}">
@@ -18,8 +18,11 @@ $(document).ready(function() {
                   <div class="card-content textcolor cardcolor">
                     <p class="textcolor">${theQuote}</p>
                     <p class="textcolor"><i>-Ron Swanson</i></p>
+
                   </div>
                 </div>
+                <a class="waves-effect waves-light btn btncolor textcolor" id="thebutton">I need more ron</a>
+
               </div>
             </div>`)
       })
